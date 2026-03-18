e6637bf3f485ba1fdb66fcd7a1541f8ac187e972
Ashwathi <as6239@srmist.edu.in>
2026-03-18 09:41:51 +0530

    added UC2

---
+++ Helloapp.java
@@ -0,0 +1,13 @@
+/*public class Helloapp {
+public static void main(String[] args) {
+System.out.println("Hello, World!");
+}
+}*/
+
+public class Helloapp {
+    public static void main(String[] args) {
+        
+        String name = args[0]; 
+        System.out.println("Hello, " + name + "!");
+    }
+}
