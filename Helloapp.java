ba031cf4d1ae02b0f37163ad6008d1c96aaaf6b8
Ashwathi <as6239@srmist.edu.in>
2026-03-30 22:40:09 +0530

    added UC4

--- Helloapp.java
+++ Helloapp.java
@@ -1,4 +1,4 @@
-/*public class Helloapp {
+/* public class Helloapp {
 public static void main(String[] args) {
 System.out.println("Hello, World!");
 }
@@ -15,9 +15,9 @@ public class Helloapp {
 =======
 }
 >>>>>>> feature/UC2
-*/
 
-public class Helloapp {
+
+/* public class Helloapp {
 public static void main(String[] args) {
 String name = "World";
 if (args.length > 0) {
@@ -25,4 +25,19 @@ name = args[0];
 }
 System.out.println("Hello, "+ name + "!");
 }
+} */
+
+public class Helloapp {
+    public static void main(String[] args) {
+                if (args.length > 0) {
+                    String allNames = String.join(", ", args);
+                    System.out.println("Hello, " + allNames + "!");
+                    
+                } else {
+                    System.out.println("Hello, World!");
+        }
+    }
 }
+
+
+
